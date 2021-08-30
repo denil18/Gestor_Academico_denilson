21e4faa96beb6f8acc170d1205124dcd489b68c1
denil18 <acostadenilson944@gmail.com>
2021-08-30 10:39:51 -0500

    jnbkoubbñ

---
+++ Frontend/src/components/homepageComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class homepageComponent extends Component {
+  render() {
+    return (
+      <div>
+          Hola Mundo component
+      </div>
+    )
+  }
+}
+
+export default homepageComponent
---
+++ Frontend/src/components/teacherComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class teacherComponent extends Component {
+  render() {
+    return (
+      <div>
+        teacher Component
+      </div>
+    )
+  }
+}
